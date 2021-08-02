a1c1a621bef645b5e5914fcac80c6d92fa7510f5
Lewis Blackburn <lewisblackburn10@gmail.com>
2021-08-02 13:30:06 +0100

    login workflow

--- packages/frontend/src/modules/landing/LandingPage.tsx
+++ packages/frontend/src/modules/landing/LandingPage.tsx
@@ -1,11 +1,32 @@
+import { useApolloClient } from "@apollo/client";
+import { useRouter } from "next/router";
 import React from "react";
+import { useLogoutMutation } from "../../generated/graphql";
 import { useCurrentState } from "../../stores/useCurrentState";
+import { useVerifyLoggedIn } from "../auth/useVerifyLoggedIn";
 import { Layout } from "../layouts/Layout";
 
 interface LandingPageProps {}
 
 export const LandingPage: React.FC<LandingPageProps> = ({}) => {
-  const { state, setCurrentState } = useCurrentState();
+  useVerifyLoggedIn();
 
-  return <Layout></Layout>;
+  const { state, setCurrentState } = useCurrentState();
+  const router = useRouter();
+  const [logout] = useLogoutMutation();
+  const apolloClient = useApolloClient();
+
+  return (
+    <Layout>
+      <button
+        onClick={async () => {
+          await logout().then(() => router.push("/login"));
+          await apolloClient.resetStore();
+        }}
+        className="bg-purple-400 text-white px-4 py-1"
+      >
+        logout
+      </button>
+    </Layout>
+  );
 };
