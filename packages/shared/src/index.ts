08b0f281f6c1b19ff1255efdc64ab3829705222a
Lewis Blackburn <lewisblackburn10@gmail.com>
2021-10-13 17:26:33 +0100

    fix: removed minus function

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -2,8 +2,4 @@ export function add(a: number, b: number) {
   return a + b + 10;
 }
 
-export function minus(a: number, b: number) {
-  return a - b - 10;
-}
-
 export const IS_DEV = process.env.NODE_ENV === "development";
